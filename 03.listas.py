57a58df58a273109b84ca825f5573acaf422eeee
searjona <serarjona22@gmail.com>
2024-09-12 14:21:59 -0500

    listas

---
+++ 03.listas.py
@@ -0,0 +1,21 @@
+# Lista vacia
+lista_vacia: list = []
+
+# Lista con elementos
+motocicletas: list = ['Honda', 'Yamaha', 'Suzuki', 'Honda']
+
+""" 
+# Transformando los elementos de una lista en enteros con listas de compresion
+values: list = input().split(' ')
+print(values)
+values_transform: list = [int(i) for i in values]
+print(values_transform)
+
+# Transforma los datos de una lista en entero y los guarda en variables con el metodo map()
+n, m, p = map(int, values)
+print(n)
+"""
+
+# Añadiendo elementos a una lista
+motocicletas.append('Kawasaki')
+print(motocicletas)
