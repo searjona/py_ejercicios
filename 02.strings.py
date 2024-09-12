653ca95f61fad135b324e21aff131d1a7fd183a6
searjona <serarjona22@gmail.com>
2024-09-12 14:14:56 -0500

    strings

--- 02.strings.py
+++ 02.strings.py
@@ -13,6 +13,19 @@ print(cadena_espacios.strip())
 cadena_reemplazo: str = "ESTOPEESPEUNPEMENSAJEPECIFRADO"
 print(cadena_reemplazo.replace('PE', ' '))
 
-# Colocar un string en minusculas
+# Colocar un string en minusculas o mayusculas
 cadena_minuscula: str = "Esto ES UN Texto QUE saldrA en MINÚSCULAS"
 print(cadena_minuscula.lower())
+cadena_mayuscula: str = "este saldra convertido en mayúsculas"
+print(cadena_mayuscula.upper())
+
+# Comprobar si un string tiene prefijos y sufijos
+url: str = "www.r2d2.com"
+prefijo: bool = url.startswith('www')
+sufijo: bool = url.endswith('.com')
+print(prefijo)
+print(sufijo)
+
+# Encontrar la posicion del primer carácter en la primera posicion de un string, si no lo encuentra devuelve -1
+indice: int = url.find("2")
+print(indice)
