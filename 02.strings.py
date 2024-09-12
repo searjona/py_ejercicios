09aa06b598d7086a1a05ef57d04d4ef790a7688a
searjona <serarjona22@gmail.com>
2024-09-12 12:43:14 -0500

    strings

--- 02.strings.py
+++ 02.strings.py
@@ -8,3 +8,11 @@ print(len(saludo))
 # Quitar espacios en blanco --> strip()
 cadena_espacios: str = "   Texto con espacios  "
 print(cadena_espacios.strip())
+
+# Reemplazar partes de un string por otro
+cadena_reemplazo: str = "ESTOPEESPEUNPEMENSAJEPECIFRADO"
+print(cadena_reemplazo.replace('PE', ' '))
+
+# Colocar un string en minusculas
+cadena_minuscula: str = "Esto ES UN Texto QUE saldrA en MINÚSCULAS"
+print(cadena_minuscula.lower())
