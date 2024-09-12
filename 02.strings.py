a0c0ad3288f711ba6224ce93a7ce3370da985130
searjona <serarjona22@gmail.com>
2024-09-12 12:02:53 -0500

    strings methods

---
+++ 02.strings.py
@@ -0,0 +1,10 @@
+# Imprime todos metodos que se pueden usar con un string
+#print(dir(''))
+
+# Longitud de un string --> len()
+saludo: str = "Este es un curso de python"
+print(len(saludo))
+
+# Quitar espacios en blanco --> strip()
+cadena_espacios: str = "   Texto con espacios  "
+print(cadena_espacios.strip())
