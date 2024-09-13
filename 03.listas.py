36a503a215c1be34985564f10de3ea5ba7908b42
searjona <serarjona22@gmail.com>
2024-09-12 21:55:10 -0500

    lists

--- 03.listas.py
+++ 03.listas.py
@@ -16,6 +16,29 @@ n, m, p = map(int, values)
 print(n)
 """
 
-# Añadiendo elementos a una lista
+# Añade un elemento al final de una lista
 motocicletas.append('Kawasaki')
 print(motocicletas)
+
+# Añade un elemento en la posicion que se escoja en una lista
+motocicletas.insert(0, 'Husbarna')
+print(motocicletas)
+
+# Borrar un elemento de una lista (se puede guardar en una variable)
+marca_moto: str = motocicletas.pop(1)
+print(motocicletas)
+print(marca_moto)
+
+# Metodo pop() sin pasarle ningun parametro borra el ultimo elemento de la lista
+motocicletas.pop()
+print(motocicletas)
+
+# Elimina un elemento de la lista pero este no se puede guardar en una variable
+# Si este se repite elimina el primer elemento
+cars: list = ['Mustand', 'Camaro', 'Audi R8', 'Mustand']
+cars.remove('Mustand')
+print(cars)
+
+# Eliminar la lista completa
+cars.clear()
+print(cars)
